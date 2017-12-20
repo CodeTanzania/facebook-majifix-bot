7aee9fd543457fe3dcd5c6217e8e648043b95e52
kasongoyo <kasongoyo@gmail.com>
2017-12-20 23:07:11 +0300

    initial commit

---
+++ bottender.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  messenger: {
+    accessToken: process.env.PAGE_ACCESS_TOKEN,
+    verifyToken: process.env.APP_VERIFY_TOKEN,
+    appId: process.env.APP_ID,
+    appSecret: process.env.APP_SECRET_TOKEN
+  }
+};
